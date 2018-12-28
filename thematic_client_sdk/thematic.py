e4ce1453d230f3947220332ff93236697afe2d1e
Nathan Holmberg <nathan@getthematic.com>
2018-12-28 10:41:12 -0800

    PORTAL-1138: add integrations wrapper

--- thematic_client_sdk/thematic.py
+++ thematic_client_sdk/thematic.py
@@ -1,11 +1,12 @@
 # pylint: disable=too-few-public-methods
 from .data import Data
-from .visualizations import Visualizations
-from .surveys import Surveys
-from .reports import Reports
-from .upload_jobs import UploadJobs
+from .integrations import Integrations
 from .organizations import Organizations
+from .reports import Reports
+from .surveys import Surveys
+from .upload_jobs import UploadJobs
 from .views import Views
+from .visualizations import Visualizations
 
 
 class ThematicClient(object):
@@ -13,9 +14,10 @@ class ThematicClient(object):
     def __init__(self, access_token, api_url='https://client.getthematic.com/api'):
         self.api_url = api_url
         self.data = Data(access_token, api_url)
+        self.integrations = Integrations(access_token, api_url)
         self.organizations = Organizations(access_token, api_url)
-        self.visualizations = Visualizations(access_token, api_url)
-        self.surveys = Surveys(access_token, api_url)
         self.reports = Reports(access_token, api_url)
-        self.views = Views(access_token, api_url)
+        self.surveys = Surveys(access_token, api_url)
         self.upload_jobs = UploadJobs(access_token, api_url)
+        self.views = Views(access_token, api_url)
+        self.visualizations = Visualizations(access_token, api_url)
