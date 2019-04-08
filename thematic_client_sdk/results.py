8d0f336f0db83901a5e8969eff4f6a47e16b74a0
Anna Christison <anna@getthematic.com>
2019-04-09 11:33:47 +1200

    tools-54 add publishing type

--- thematic_client_sdk/results.py
+++ thematic_client_sdk/results.py
@@ -3,9 +3,9 @@ from .requester import Requestor
 
 class Results(Requestor):
 
-    def create(self, survey_id, job_id):
+    def create(self, survey_id, job_id, result_type='published'):
         url = self.create_url('/survey/{}/result'.format(survey_id))
-        fields = {'jobID': job_id, 'manualUploadAllowed': True}        
+        fields = {'jobID': job_id, 'type': result_type, 'manualUploadAllowed': True}        
         response = requests.post(
             url, headers={'Authorization': 'bearer ' + self.access_token}, json=fields)
         if response.status_code != 200:
