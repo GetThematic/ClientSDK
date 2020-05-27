535957237b0f6060e946025f0019a9040814de7e
Aaron Glover <aaron@getthematic.com>
2020-05-27 15:34:07 +1200

    PORTAL-2159 adjust examples to suit

--- example/pull_data_anz.py
+++ example/pull_data_anz.py
@@ -8,7 +8,7 @@ from thematic_client_sdk import Auth, ThematicClient
 def main():
     # get token and args from command line
     if len(sys.argv) < 3:
-        print("Usage: "+sys.argv[0]+' <refresh_token> <survey_id> <result_id>')
+        print("Usage: "+sys.argv[0]+' <refresh_token> <survey_id> <output_format>')
         exit()
     refresh_token = sys.argv[1]
     survey_id = sys.argv[2]
@@ -27,11 +27,15 @@ def main():
     # get the processed file
     try:
         save_location = str(survey_id)+'_'+str(result_id)
-        client.data.download_data(save_location+'_processed.csv',
+        filename = save_location+'_processed.csv'
+        print('saving data to {}'.format(filename))
+        client.data.download_data(filename,
                                   survey_id,
                                   result_id=result_id,
                                   output_format=output_format)
-        client.data.download_themes(save_location+'_processed.json', survey_id, result_id=result_id)
+        filename = save_location+'_processed.json'
+        print('saving themes to {}'.format(filename))
+        client.data.download_themes(filename, survey_id, result_id=result_id)
     except Exception as exc:
         print("Failed to get results: "+str(exc))
         exit()
