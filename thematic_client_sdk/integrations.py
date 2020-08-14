a3eb15e75317e292d24fc3566abef9665ee7c901
Nathan Holmberg <nathan@getthematic.com>
2020-08-14 16:59:11 +1200

    PORTAL-2403: Add function to all for the updating of integration info

--- thematic_client_sdk/integrations.py
+++ thematic_client_sdk/integrations.py
@@ -31,3 +31,20 @@ class Integrations(Requestor):
                 'Could not retrieve integrations: '+str(response.text))
         integrations = response.json()['data']
         return integrations
+
+    def update(self, integration_id, integration_details, is_new_integration=True):
+        '''
+        Retrieves info on specific integration
+        '''
+        url = self.create_url('/integration/{}?isNewIntegration={}'.format(integration_id, str(is_new_integration)))
+        fields = {
+            'authInfo': integration_details
+        }
+
+        response = requests.put(
+            url, headers={'Authorization': 'bearer ' + self.access_token}, json=fields)
+        if response.status_code != 200:
+            raise Exception(
+                'Could not retrieve integrations: '+str(response.text))
+        integrations = response.json()['data']
+        return integrations
